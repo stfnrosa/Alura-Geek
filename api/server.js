bb4eae0f4ec4df3f0cb24ea40690ac38808bc7c2
stfnrosa <rosaa.estefanii@gmail.com>
2024-05-24 12:18:16 -0300

    feat

--- api/server.js
+++ api/server.js
@@ -11,20 +11,17 @@ server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(router);
 
-// Ruta para crear un nuevo producto
 server.post("/produtos", (req, res) => {
   const novoProduto = req.body;
   router.db.get("produtos").push(novoProduto).write();
   res.send(novoProduto);
 });
 
-// Ruta para obtener todos los produtos
 server.get("/produtos", (req, res) => {
   const produtos = router.db.get("produtos").value();
   res.send(produtos);
 });
 
-// Ruta para actualizar un producto por su ID
 server.put("/produtos/:id", (req, res) => {
   const idProducto = req.params.id;
   const newData = req.body;
@@ -32,7 +29,6 @@ server.put("/produtos/:id", (req, res) => {
   res.sendStatus(200);
 });
 
-// Ruta para eliminar un producto por su ID
 server.delete("/produtos/:id", (req, res) => {
   const idProducto = req.params.id;
   router.db.get("produtos").remove({ id: idProducto }).write();
