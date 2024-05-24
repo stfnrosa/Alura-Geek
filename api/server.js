2466360eefc2f27f7fec75742740cd747cf2bb57
stfnrosa <rosaa.estefanii@gmail.com>
2024-05-24 14:21:21 -0300

    vercel

--- api/server.js
+++ api/server.js
@@ -1,40 +1,24 @@
+// JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1'
-}))
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/*": "/$1",
+ })
+);
 server.use(router);
-
-server.post("/produtos", (req, res) => {
-  const novoProduto = req.body;
-  router.db.get("produtos").push(novoProduto).write();
-  res.send(novoProduto);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
 });
 
-server.get("/produtos", (req, res) => {
-  const produtos = router.db.get("produtos").value();
-  res.send(produtos);
-});
-
-server.put("/produtos/:id", (req, res) => {
-  const idProducto = req.params.id;
-  const newData = req.body;
-  router.db.get("produtos").find({ id: idProducto }).assign(newData).write();
-  res.sendStatus(200);
-});
-
-server.delete("/produtos/:id", (req, res) => {
-  const idProducto = req.params.id;
-  router.db.get("produtos").remove({ id: idProducto }).write();
-  res.sendStatus(200);
-});
-
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+// Export the Server API
+module.exports = server;
